bb44f0e304b3d55183bd3f7901ccc91a3ed52421
Maxwell Oteng <maxwelloteng@Maxwells-MacBook-Pro-2.local>
2020-10-22 13:05:11 -0700

    updated js

--- Leaflet challenge/Leaflet-Step-1/static/js/logic.js
+++ Leaflet challenge/Leaflet-Step-1/static/js/logic.js
@@ -15,8 +15,23 @@ function createFeatures(earthquakeData){
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
 
 }
-/ Create a GeoJSON layer containing the features array on the earthquakeData object
+// Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature
   });
+  // Create map function and send earthquakes layer to the function
+  createMap(earthquakes);
+}
+function createMap(earthquakes){
+  // Define map layers
+  var airmap = = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
+
+}
