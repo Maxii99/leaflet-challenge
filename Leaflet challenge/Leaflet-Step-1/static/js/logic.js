7b09d8952620c2dc28dabc47d564c1a537e7ae44
Maxwell Oteng <maxwelloteng@Maxwells-MacBook-Pro-2.local>
2020-10-21 21:24:25 -0700

    commit

--- Leaflet challenge/Leaflet-Step-1/static/js/logic.js
+++ Leaflet challenge/Leaflet-Step-1/static/js/logic.js
@@ -7,3 +7,16 @@ d3.json(earthquakeURL, function(data) {
   // Send the data.features object to the createFeatures function
   createFeatures(data.features);
 });
+// Define function to run for each feature in the features array
+function createFeatures(earthquakeData){
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+
+}
+/ Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });
