7bb24768d01605cd599435395e8d7f5a183eb606
Maxwell Oteng <maxwelloteng@Maxwells-MacBook-Pro-2.local>
2020-10-23 18:38:38 -0700

    commit

--- Leaflet challenge/Leaflet-Step-1/static/js/logic.js
+++ Leaflet challenge/Leaflet-Step-1/static/js/logic.js
@@ -1,9 +1,12 @@
+// Create a variable for maop
+var myMap;
 // Store given API endpoint in queryURL
-var earthquakeURL = https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson;
-var tectonicURL = https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json;
+var earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var tectonicURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
 
-// Perform a GET request to the  earthquakeURL
+// Perform a GET request to the earthquakeURL
 d3.json(earthquakeURL, function(data) {
+  console.log(data);
   // Send the data.features object to the createFeatures function
   createFeatures(data.features);
 });
@@ -25,7 +28,7 @@ function createFeatures(earthquakeData){
 }
 function createMap(earthquakes){
   // Define map layers
-  var airmap = = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var airmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
@@ -33,5 +36,37 @@ function createMap(earthquakes){
     id: "mapbox/streets-v11",
     accessToken: API_KEY
   });
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
+
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Air Map": airmap,
+    "Dark Map": darkmap
+  };
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+ // Create our map, giving it the streetmap and earthquakes layers to display on load
+ var myMap = L.map("map", {
+  center: [
+    37.09, -95.71
+  ],
+  zoom: 5,
+  layers: [airmap, earthquakes]
+});
+
+// Create a layer control
+// Pass in our baseMaps and overlayMaps
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+}).addTo(myMap);
 
 }
